3d3b3884543b5e10de9b2591bcf0f986d57ee02c
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-08-01 14:50:52 +0500

    Cards and Chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello, its working!
-    </div>
-  );
+import { Cards, CountryPicker, Chart } from './components'
+import {fetchData} from './components/Api'
+
+import styles from './App.module.css';
+
+
+export class App extends Component {
+
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+    this.setState({ data: fetchedData })
+  }
+  render() {
+    const data = this.state.data
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import { fetchDailyData } from '../Api'
+
+import { Line, Bar } from 'react-chartjs-2'
+
+const Chart = () => {
+
+    const [ dailyData, setDailyData ] = useState([])
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData())
+        }
+
+        fetchAPI() 
+        
+    }, [])
+
+    const lineChart = (
+        dailyData.length ? 
+        (<Line
+            data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [{
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true
+                }, {
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)' ,
+                    fill: true
+                }]
+            }}
+        />) : null
+    )
+
+    return (
+        <div>
+            {lineChart}
+        </div>
+    )
+}
+
+export default Chart
